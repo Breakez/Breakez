a5736f65bec2237314f1a5eb0a84ebc531e1eaa7
Breakez <92742627+Breakez@users.noreply.github.com>
2021-10-23 17:53:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">"
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Login Form</title>
+</head>
+<body>
+    <div class="container">
+        <form class="login-email">
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+            <div class="input-group">
+                <input type="email" placeholder="Email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="input-group">
+                <button class="btn">Login</button>
+            </div>
+            <p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
